6931ea26ad474e5819e751c6e530101394e9318a
MahmoodMI1 <mahmood.idelbi1@icloud.com>
2026-06-02 10:07:02 -0400

    chore: added entities validations

--- src/main/java/kiritsu/resource/subscriptions/Subscriptions.java
+++ src/main/java/kiritsu/resource/subscriptions/Subscriptions.java
@@ -1,6 +1,7 @@
 package kiritsu.resource.subscriptions;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,12 +23,20 @@ public class Subscriptions {
 
     private String user_sub;
 
+    @NotBlank
+    @Size(min = 2, max = 50)
     private String name;
 
+    @NotNull
+    @Positive
+    @DecimalMax("9999.99")
     private float price;
 
     private enum category {
         gaming, streaming, learning, AI, phone, cloud
+    };
+    private enum priority {
+        worth, not_worth
     };
 
     @CreatedDate
