7c84b2145bccd3c433b0a7d8e6116863195bb876
MahmoodMI1 <mahmood.idelbi1@icloud.com>
2026-06-02 12:58:44 -0400

    fix: added a table annotation to match the name of the entity and migration

--- src/main/java/kiritsu/resource/subscriptions/Subscription.java
+++ src/main/java/kiritsu/resource/subscriptions/Subscription.java
@@ -19,6 +19,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Entity
 @EntityListeners(AuditingEntityListener.class)
+@Table(name = "subscriptions")
 public class Subscription {
 
     @Id
